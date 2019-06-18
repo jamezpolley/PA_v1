da0b43dd3912cb47136e068db5cf0fbdee9ceb5f
lowndsy <lowndsy@hotmail.com>
2019-06-18 14:27:50 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -24,18 +24,6 @@ echo $success.".
 ";
 
 
-$url = "https://api.morph.io/lowndsy/PA-v7/data.json";
-$response=file_get_contents($url.'?key='.$key.'&query='.urlencode($query));
-$js=json_decode($response,true);
-echo count($js)." - ";
-foreach ($js as $line)
-{
-scraperwiki::save(array('prikey'), $line);
-$success++; 
-}
-unset($js);
-echo $success.". 
-";
 
 echo 'Complete';
 ?>
