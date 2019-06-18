39525c370a3bf2b670a3a1f55dc1be1da7682b73
lowndsy <lowndsy@hotmail.com>
2019-06-18 13:42:57 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -5,7 +5,7 @@ require 'scraperwiki.php';
 
 $apikey = getenv('MORPH_API_KEY');
 $key=$apikey;
-$query="select * from 'data'";
+$query="select * from 'data' limit 30";
 
 $success=0;
 
@@ -13,6 +13,7 @@ $success=0;
 $url = "https://api.morph.io/lowndsy/PA-v7/data.json";
 $response=file_get_contents($url.'?key='.$key.'&query='.urlencode($query));
 $js=json_decode($response,true);
+print_r($js);
 echo count($js)." - ";
 foreach ($js as $line)
 {
