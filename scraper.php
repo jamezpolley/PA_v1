e34a4b8e495a3da219ec99de4a805a8118440894
lowndsy <lowndsy@hotmail.com>
2019-06-18 14:16:29 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -23,47 +23,5 @@ unset($js);
 echo $success.". 
 ";
 
-$url = "https://api.morph.io/lowndsy/PA-v6/data.json";
-$response=file_get_contents($url.'?key='.$key.'&query='.urlencode($query));
-$js=json_decode($response,true);
-echo count($js)." - ";
-foreach ($js as $line)
-{
-scraperwiki::save(array('prikey'), $line);
-$success++; 
-}
-unset($js);
-echo $success.". 
-";
-
-
-$url = "https://api.morph.io/lowndsy/PA-v8/data.json";
-$response=file_get_contents($url.'?key='.$key.'&query='.urlencode($query));
-$js=json_decode($response,true);
-echo count($js)." - ";
-foreach ($js as $line)
-{
-scraperwiki::save(array('prikey'), $line);
-$success++;
-}
-unset($js);
-echo $success.". 
-";
-
-
-$url = "https://api.morph.io/lowndsy/PA-v9/data.json";
-$response=file_get_contents($url.'?key='.$key.'&query='.urlencode($query));
-$js=json_decode($response,true);
-echo count($js)." - ";
-foreach ($js as $line)
-{
-scraperwiki::save(array('prikey'), $line);
-$success++;
-}
-unset($js);
-echo $success.". 
-";
-
-
 echo 'Complete';
 ?>
