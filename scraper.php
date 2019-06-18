afee955362b4b92ce517387125be293e1af7c019
lowndsy <lowndsy@hotmail.com>
2019-06-18 11:27:24 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -28,41 +28,52 @@ $key=$apikey;
 $query="select * from 'data'";
 
 
+
+
 $url = "https://api.morph.io/lowndsy/PA-v6/data.json";
 $response=file_get_contents($url.'?key='.$key.'&query='.urlencode($query));
 $js=json_decode($response,true);
-
 foreach ($js as $line)
 {
  scraperwiki::save(array('prikey'), $line);
 }
+
+
+
 
 $url = "https://api.morph.io/lowndsy/PA-v7/data.json";
 $response=file_get_contents($url.'?key='.$key.'&query='.urlencode($query));
 $js=json_decode($response,true);
-
 foreach ($js as $line)
 {
  scraperwiki::save(array('prikey'), $line);
 }
+
+
+
+
 
 $url = "https://api.morph.io/lowndsy/PA-v8/data.json";
 $response=file_get_contents($url.'?key='.$key.'&query='.urlencode($query));
 $js=json_decode($response,true);
-
 foreach ($js as $line)
 {
  scraperwiki::save(array('prikey'), $line);
 }
+
+
+
+
 
 $url = "https://api.morph.io/lowndsy/PA-v9/data.json";
 $response=file_get_contents($url.'?key='.$key.'&query='.urlencode($query));
 $js=json_decode($response,true);
-
 foreach ($js as $line)
 {
  scraperwiki::save(array('prikey'), $line);
 }
+
+
 
 echo 'Complete';
 ?>
